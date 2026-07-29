6b794269ea2f3092c8b15a23f8140e3c0c97a41b
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-30 00:00:12 +0500

    Enhance logging to include request URLs in HTTP server

--- NodeJS/Day-2/index.js
+++ NodeJS/Day-2/index.js
@@ -2,7 +2,7 @@ const http = require("http")
 const fs = require("fs")
 const myServer = http.createServer((req,res)=>{
     // res.end("Hello From NodeJs")
-    const log = `${Date.now()}: New Request Receive\n`
+    const log = `${Date.now()}: & From ${req.url} New Request Receive\n`
     fs.appendFile("log.txt", log, (err)=>{
         if (err) {
             console.log(err);
