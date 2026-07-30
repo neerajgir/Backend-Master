e99836bd8bf1520ef1f303ee50deb107273156a8
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-31 00:54:49 +0500

    Add path utilities to enhance file path handling and logging in index.js

--- NodeJS/Day-3/index.js
+++ NodeJS/Day-3/index.js
@@ -9,3 +9,17 @@ console.log("Dirname:", __dirname)
 const filepath = path.join("folder", "students", "data.txt");
 
 console.log(filepath)
+
+const parsedDataPath = path.parse(filepath)
+const resolvePath = path.resolve(filepath)
+const extname = path.extname(filepath)
+const basename = path.basename(filepath)
+const dirname = path.dirname(filepath)
+
+console.log({
+    resolvePath,
+    extname,
+    basename,
+    dirname,
+    parsedDataPath
+})
