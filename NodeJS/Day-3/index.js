66087fab4fbc5a033930b2576234639eec609176
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-31 01:13:14 +0500

    Update event emitter in index.js to include user details in greeting message

--- NodeJS/Day-3/index.js
+++ NodeJS/Day-3/index.js
@@ -6,8 +6,11 @@ const emitter = new eventEmitter();
 //? on(eventname, listen) = create
 //? emit(eventname, [args]) = execute
 
-emitter.on("Greet", ()=>{
-    console.log("Hello Word!")
+emitter.on("Greet", (args)=>{
+    console.log(`Hello Word! ${args.username} and the id is: ${args.id}`)
 })
 
-emitter.emit("Greet")
+emitter.emit("Greet", {
+    username: "Neeraj",
+    id: "3223krkfksdjrj4jl4kl"
+})
