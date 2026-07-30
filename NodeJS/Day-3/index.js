39e4189c9475cfab50f06433e867e54f3ae6222e
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-31 01:39:01 +0500

    Add event tracking and summary functionality in index.js to log user actions

--- NodeJS/Day-3/index.js
+++ NodeJS/Day-3/index.js
@@ -1,22 +1,60 @@
-const eventEmitter = require("events");
+//Tasks
 
+const eventEmitter = require("events");
 const emitter = new eventEmitter();
 
+const fs = require("fs")
+
+const eventsCounts = {
+    login: 0,
+    logout: 0,
+    purchase: 0,
+    profile_update: 0
+}
+
+const logFile = "eventlog.json"
+
+
+function saveCounts() {
+    fs.writeFileSync(logFile, JSON.stringify(eventsCounts, null, 2))
+}
+
+if(fs.existsSync(logFile)){
+  const data = fs.readFileSync(logFile, "utf-8")
+  Object.assign(eventsCounts, JSON.parse(data))   
+}
+
 emitter.on("login", (username)=>{
+    eventsCounts.login++;
     console.log(`${username} loggedIn successfully!`)
+    saveCounts()
 })
 emitter.on("logout", (username)=>{
+    eventsCounts.logout++;
     console.log(`${username} loggedOut successfully!`)
+    saveCounts()
 })
 emitter.on("purchase", (username, item)=>{
+    eventsCounts.purchase++;
     console.log(`${username} purchased ${item}!`)
+    saveCounts()
 })
 emitter.on("profile_update", (username, field)=>{
+    eventsCounts.profile_update++;
     console.log(`${username} update their profile and ${field}!`)
+    saveCounts()
 })
 
+emitter.on("summary", ()=>{
+    console.log("\n Event Summary:")
+    console.log(`Logins: ${eventsCounts.login}`)
+    console.log(`Logouts: ${eventsCounts.logout}`)
+    console.log(`Purchases: ${eventsCounts.purchase}`)
+    console.log(`Profile_Updates: ${eventsCounts.profile_update}`)
+})
 
 emitter.emit("login", "Neeraj")
 emitter.emit("logout", "Neeraj")
 emitter.emit("purchase", "Neeraj", "Samsung S26 ultra")
 emitter.emit("profile_update", "Neeraj", "DP and Password")
+emitter.emit("summary")
