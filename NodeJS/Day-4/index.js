468a7aa0a1d6b82d923d0f06742dc7a0ea8b2c6b
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-31 14:51:00 +0500

    Sends data in good-way in chunks

--- NodeJS/Day-4/index.js
+++ NodeJS/Day-4/index.js
@@ -2,11 +2,14 @@ const http = require("http");
 const fs = require("fs")
 const server = http.createServer((req,res)=>{
     //? Downloading file in bad way.
-    const read = fs.readFileSync("text.txt");
-
-    res.end(read)
+    // const read = fs.readFileSync("text.txt");
+    //?Downloading file in good way.
+    const readableStreams = fs.createReadStream("text.txt")
+    readableStreams.pipe(res)
+    // res.end()
 })
 
 server.listen(3000, ()=>{
     console.log("Server is running on 3000.")
 })
+
