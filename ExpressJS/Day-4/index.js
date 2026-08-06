80a2cbbcaea51b4745d8e76ab7b952f11356c28c
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-06 17:03:46 +0500

    Add session management and cookie parsing in index.js; update package.json and package-lock.json to include cookie-parser dependency.

--- ExpressJS/Day-4/index.js
+++ ExpressJS/Day-4/index.js
@@ -1,9 +1,24 @@
+import cookieParser from "cookie-parser";
 import express from "express";
-
+import session from "express-session";
 
 const app = express();
 const PORT = 3000;
 
+app.use(session({
+    secret: "OT/8/pn7PakW4mKEVeQIuDtV7VhvKxnp",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {maxAge: 1000*60*60*24}
+}));
+
+app.use(cookieParser("fmJ4Ahni7oXThnfWZ41khtUCPTnyPHIO9DRMZ2SYIBg="))
+
+app.get("/", (req,res)=>{
+    console.log(req.session);
+    console.log(req.session.id);
+    res.send("Hello World")
+})
 
 
 
