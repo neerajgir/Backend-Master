c74af0bb19fcd40da3ad7746bf57b2591f40a6d0
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-15 02:01:07 +0500

    Add task creation functionality in task.controller.js, including error handling and response formatting for successful task creation, enhancing task management capabilities.

--- Authentication/controllers/task.controller.js
+++ Authentication/controllers/task.controller.js
@@ -1,5 +1,21 @@
 
 
-export const addTask = async ()=>{}
+export const addTask = async (req,res)=>{
+    try {
+        const {title, description} = req.body;
+        const task =  await createTask(req.session.userId, title, description);
+        req.status(201).json({
+            success: true,
+            message: "Task Created Successfully.",
+            data: task
+        })
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message: "Error in created task",
+            error: error.message
+        })
+    }
+}
 
 export const fetchTasks = async ()=>{}
