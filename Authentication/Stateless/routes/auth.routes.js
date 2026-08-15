8db4cb6140f07909fc8a5763ad65689b99999d1c
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-16 01:08:09 +0500

    Add login route to authentication module in Stateless, implementing JWT token generation and error handling for user login. Enhance user experience with appropriate success and error responses.

--- Authentication/Stateless/routes/auth.routes.js
+++ Authentication/Stateless/routes/auth.routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import jwt from 'jsonwebtoken'
 import User from '../models/user.model.js'
 const router = express.Router();
 
@@ -31,6 +32,27 @@ router.post("/signup", async (req,res) => {
 })
 
 //login
+router.post("/login", async (req,res) => {
+    try {
+        const {username, password} = req.body;
 
+        const user = await User.findOne({username});
+        if(!user) return res.status(401).json({success:false, message: "Invalid username or password"})
+        const isMatch = await user.comparePassword(password)
+    if(!isMatch) return res.status(400).json({success: false, message: "Invalid username or password"})
+        const token = jwt.sign({id: user._id, username:user.username}, process.env.JWT_SECRET, {expiresIn: "1h"})
+
+    res.status(200).json({
+        success: true,
+        message: "User login successfully.",
+        token
+    })
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong",
+            error: error.message
+        })
+    }
+})
 
 export default router;
