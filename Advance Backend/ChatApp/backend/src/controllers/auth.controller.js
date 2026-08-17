a9b7c86b95816652954d53f9ace0f16a7a8cb935
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-18 03:03:22 +0500

    Implement login functionality in auth.controller.js with enhanced error handling and user feedback. Validate user credentials and return appropriate responses for successful and failed login attempts.

--- Advance Backend/ChatApp/backend/src/controllers/auth.controller.js
+++ Advance Backend/ChatApp/backend/src/controllers/auth.controller.js
@@ -69,9 +69,31 @@ export const signup = async (req,res)=>{
 
 export const login = async (req,res) => {
     try {
-        
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(400).json({message: "Invalid Credentials"})
+        }
+
+        const isPasswordCorrect  = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message: "Invalid Password"})
+        }
+
+        generateToken(user._id, res);
+        res.status(200).json({
+            message: "User Login Successfully",
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic  
+        })
     } catch (error) {
-        
+        return res.status(500).json({ 
+            success: false, 
+            message: "Internal server error",
+            error: error.message 
+        });
     }
 }
 
