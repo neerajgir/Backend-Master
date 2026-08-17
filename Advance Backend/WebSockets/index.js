2576afe18a42d4ebe083cc580c1b5e3a0ad86679
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-18 00:18:07 +0500

    Add WebSocket server functionality to handle connections and messages in index.js, enhancing real-time communication capabilities. Update HTTP response handling for incoming requests.

--- Advance Backend/WebSockets/index.js
+++ Advance Backend/WebSockets/index.js
@@ -1,9 +1,32 @@
 import http from 'http';
+import WebSocket, {WebSocketServer} from "ws";
 
 const server = http.createServer((req,res)=>{
     console.log((new Date()) + "received req for" + req.url)
-    req.end("Hi there")
+    res.end("Hi there")
 })
+
+// step-1
+const wss = new WebSocketServer({server})
+
+// step-2
+
+wss.on("connection", function connection(ws){
+    ws.on("error", console.error)
+
+    ws.on("message", function message(data, isBinary){
+        wss.clients.forEach(function each(client){
+            if(client.readyState === WebSocket.OPEN){
+                client.send(data, {binary: isBinary})
+            }
+        })
+    })
+
+    ws.send("Hello connection message from ws server")
+})
+
+
+
 
 server.listen(3000, ()=>{
     console.log("Server is running on 3000 Port");
