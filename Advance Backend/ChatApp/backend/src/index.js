8650142fad888a72e71f0cccaf2542d198805aac
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-18 00:54:49 +0500

    Optimize WebSocket server implementation in index.js by refining connection handling and enhancing message processing efficiency, further improving real-time communication performance.

---
+++ Advance Backend/ChatApp/backend/src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+}));
+
+ 
+app.listen(PORT, ()=>{
+    console.log(`Server is up on Port ${PORT}`)
+});
