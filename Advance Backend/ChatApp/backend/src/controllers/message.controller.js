0fc5892b65f30bdd03fbeef2dc3d479d65ab117a
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-18 22:41:25 +0500

    Update message routes to include controller functions for fetching users and sending messages. Protect routes with authentication middleware for enhanced security.

---
+++ Advance Backend/ChatApp/backend/src/controllers/message.controller.js
@@ -0,0 +1,27 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+import cloudinary from "../config/cloudinary.js";
+
+export const getUsersForSidebar = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const getMessages = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const sendMessages = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
--- Advance Backend/ChatApp/backend/src/routes/message.routes.js
+++ Advance Backend/ChatApp/backend/src/routes/message.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import protectRoute from "../middlewares/auth.middleware.js"
+import { getMessages, getUsersForSidebar, sendMessages } from "../controllers/message.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js"; 
+
 
 const router = express.Router();
 
-router.get("/users");
-router.get("/:id");
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
-router.post("send/:id");
+router.post("send/:id", protectRoute, sendMessages);
 
 export default router;
