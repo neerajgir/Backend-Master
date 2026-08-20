d90ba8516a070dae627c9ba8597b55e2ab45ec65
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-21 01:17:18 +0500

    feat: update Redis client initialization and add string manipulation example

--- Advance Backend/Redis/client.js
+++ Advance Backend/Redis/client.js
@@ -1,5 +1,9 @@
-import redis from "ioredis";
+import Redis from "ioredis";
 
-const client = new redis();
+const client = new Redis({
+    port: 6379,
+    host: '127.0.0.1',
+    db: 0
+});
 
 export default client;
---
+++ Advance Backend/Redis/strings.js
@@ -0,0 +1,19 @@
+import client from "./client.js";
+
+async function init() {
+    // 1. Fetching non-existent key "msg:1" returns null initially
+    const result = await client.get("msg:1");
+    console.log("Initial Result (msg:1) -->", result);
+
+    // 2. Set the key for future executions
+    await client.set("msg:1", "Hey Redis, from node");
+
+    // 3. Fetching non-existent key "msg:6" returns null
+    const msg = await client.get("msg:6");
+    console.log("Message (msg:6) -->", msg);
+
+    // 4. Gracefully close the connection loop
+    client.disconnect();
+}
+
+init();
