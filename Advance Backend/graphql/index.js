d1131fe52a6f121e0dad1b80b6722fca7caafcb1
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-22 02:01:01 +0500

    feat: update GraphQL schema to define User and Todo types with corresponding queries

--- Advance Backend/graphql/index.js
+++ Advance Backend/graphql/index.js
@@ -3,19 +3,36 @@ import {ApolloServer} from '@apollo/server'
 import { expressMiddleware } from '@as-integrations/express5';
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import axios from "axios";
 
 async function startServer() {
     const app = express();
     const typeDefs = `
-    type Query{
-    hello:String
-    name:String
+    type User {
+    id: ID!
+    name: String!
+    username: String!
+    email: String!
+    phone: String!
+    website: String!
+    }
+
+    type Todo {
+    id: ID!
+    title: String
+    completed: Boolean 
+    }
+
+    type Query {
+    getTodos: [Todo]
+    getUsers: [User]
     }
     `
     const resolvers = {
         Query:{
-            hello:()=>"Hello World",
-            name:()=>"Neeraj"
+            getTodos: async ()=> (await axios.get("https://jsonplaceholder.typicode.com/todos")).data,
+
+            getUsers: async ()=> (await axios.get("https://jsonplaceholder.typicode.com/users")).data
         }
     }
     const server = new ApolloServer({typeDefs, resolvers})
