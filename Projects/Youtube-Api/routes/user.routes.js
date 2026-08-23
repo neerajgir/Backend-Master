1c224c82470327dce1741cc02d8d3a9b18664c12
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-23 22:26:20 +0500

    feat: implement user authentication controller with signup and login functionality

--- Projects/Youtube-Api/routes/user.routes.js
+++ Projects/Youtube-Api/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {signup, login} from "../controllers/user.controllers.js"
+import {signup, login} from "../controllers/user.controller.js"
 
 const router = express.Router();
 
---
+++ Projects/Youtube-Api/routes/video.routes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import mongoose from "mongoose";
+
+import User from "../models/user.model.js";
+import Video from "../models/video.model.js";
+import cloudinary from "../config/cloudinary.js";
+
+ 
