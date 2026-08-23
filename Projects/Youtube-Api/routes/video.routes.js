68b5614161ba75f1c03224aca20cf85c99ddac31
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-23 23:26:36 +0500

    feat: add update video functionality to video routes

--- Projects/Youtube-Api/routes/video.routes.js
+++ Projects/Youtube-Api/routes/video.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { upload } from "../controllers/video.controller.js";
+import { upload, updateVideo } from "../controllers/video.controller.js";
 import {checkAuth} from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 // router.use(fileUpload({ useTempFiles: true }));
 
 router.post("/upload", checkAuth, upload)
+router.put("/upload/:id", checkAuth, updateVideo)
 
 export default router;
