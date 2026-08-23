cff1bd91981f369b48378aaca7193a68ac42e0d5
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-23 21:19:38 +0500

    feat: implement user login functionality with JWT authentication

--- Projects/Youtube-Api/controllers/user.controllers.js
+++ Projects/Youtube-Api/controllers/user.controllers.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import cloudinary from "../config/cloudinary.js"
 
@@ -34,3 +35,62 @@ export const signup = async (req, res) => {
     }
 };
 
+//login
+
+export const login = async (req,res) => {
+    try {
+        const existingUser = await User.findOne({email: req.body.email});
+        if(!existingUser) return res.status(404).json({message: "User not found."});
+
+        const isValid = await bcrypt.compare(req.body.password, existingUser.password);
+        if(!isValid) return res.status(500).json({message: "Invalid Credentials"});
+
+        const token = jwt.sign({
+            _id: existingUser._id,
+            channelName: existingUser.channelName,
+            email: existingUser.email,
+            phone: existingUser.phone,
+            logoId: existingUser.logoId
+        }, process.env.JWT_SECRET, {expiresIn: "7d"})
+
+        res.status(200).json({
+            message: "User login successfully",
+            _id: existingUser._id,
+            channelName: existingUser.channelName,
+            email: existingUser.email,
+            phone: existingUser.phone,
+            logoId: existingUser.logoId,
+            logoUrl:existingUser.logoUrl,
+            token:token,
+            subscribers:existingUser.subscribers,
+            subscribedChannels:existingUser.subscribedChannels
+
+        })
+
+    } catch (error) {
+        console.error("Login error:", error);
+        res.status(500).json({ error: "Login failed", details: error.message });
+    }
+}
+
+
+//update-profile
+
+export const updateProfile = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.error("Update-profile error:", error);
+        res.status(500).json({ error: "Update-profile failed", details: error.message });
+    }
+}
+
+//subscribe
+
+export const subscribe = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
--- Projects/Youtube-Api/routes/user.routes.js
+++ Projects/Youtube-Api/routes/user.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {signup} from "../controllers/user.controllers.js"
+import {signup, login} from "../controllers/user.controllers.js"
 
 const router = express.Router();
 
 
 router.post("/signup", signup)
+router.post("/login", login)
 
 
 export default router;
