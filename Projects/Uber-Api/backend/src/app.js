edba79e8aedc0ddf097eebf0d3cb3721a4a66605
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-24 23:27:33 +0500

    feat: add user routes and integrate middleware in app.js

--- Projects/Uber-Api/backend/src/app.js
+++ Projects/Uber-Api/backend/src/app.js
@@ -1,14 +1,23 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import userRoutes from "./routes/user.routes.js"
 
 const app = express();
 
-
+//Middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use(cors())
+app.use(cookieParser())
 
 app.get("/", (req,res)=>{
     res.send("hello")
 })
+
+//Routes
+
+app.use("/api/v1/users", userRoutes)
 
 export default app;
