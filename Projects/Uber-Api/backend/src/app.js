13a80ab1a1bfbcee3614ca069f1f3f1ce399f1aa
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-24 23:22:52 +0500

    feat: refactor server setup to use http.createServer and separate app logic into app.js

---
+++ Projects/Uber-Api/backend/src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.get("/", (req,res)=>{
+    res.send("hello")
+})
+
+export default app;
--- Projects/Uber-Api/backend/src/server.js
+++ Projects/Uber-Api/backend/src/server.js
@@ -1,16 +1,13 @@
-import express from "express";
+import http from "http";
+import app from "./app.js";
 import dotenv from "dotenv";
 dotenv.config()
 
-
-const app = express();
+const server = http.createServer(app);
 const PORT = process.env.PORT
 
-app.get("/", (req,res)=>{
-    res.send("hello")
-})
 
 
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
     console.log(`Server is running on Port ${PORT}`)
 })
