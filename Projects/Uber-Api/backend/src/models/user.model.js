3a4a0915d0bde0eb40e25f11d3f6532779c9a7ea
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-25 00:52:42 +0500

    feat: add user model with authentication methods and validation

---
+++ Projects/Uber-Api/backend/src/models/user.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [3, "Firstname must be at least 3 characters long"],
+        },
+        lastname: {
+            type: String,
+            minlength: [3, "Lastname must be at least 3 characters long"],
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: [5, "Email must be at least 5 characters long"],
+        unique: true,
+        match: [/\S+@\S+\.\S+/, "Email is invalid"],
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    socketId: {
+        type: String
+    }
+})
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: "24h",
+    });
+    return token;
+}
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.hashPassword = async function (password) {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+}
+
+const userModel = mongoose.model("user", userSchema);
+
+export default userModel; 
