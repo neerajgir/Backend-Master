2b6589fa1eef02b523a2fe65557322f23a851f31
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-25 15:51:17 +0500

    feat: add authentication middleware to protect user profile and logout routes

---
+++ Projects/Uber-Api/backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,22 @@
+import userModel from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const authUser = async (req, res, next) => {
+    try {
+    const token = req.cookies.token || req.header("Authorization")?.replace("Bearer ", "");
+    if(!token){
+        return res.status(401).json({message: "Access denied. No token provided"});        
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await userModel.findById(decoded._id).select("-password");
+    if(!user){
+        return res.status(404).json({message: "User not found"});
+    }
+    req.user = user;
+    next();
+    }catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
--- Projects/Uber-Api/backend/src/routes/user.routes.js
+++ Projects/Uber-Api/backend/src/routes/user.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { registerUser, loginUser, getUserProfile, logoutUser } from "../controllers/user.controller.js";
 import { registerValidation, loginValidation } from "../validation/user.validation.js";
+import { authUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerValidation, registerUser);
 router.post("/login", loginValidation, loginUser);
-router.get("/profile", getUserProfile);
-router.get("/logout", logoutUser);
+router.get("/profile", authUser, getUserProfile);
+router.get("/logout", authUser, logoutUser);
 
 export default router;
