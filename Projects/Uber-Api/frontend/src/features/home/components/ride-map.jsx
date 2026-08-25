92368c1de255c089dc7ad01f8b284417b460aaca
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-26 01:14:53 +0500

    feat: update RideMap component with correct default coordinates and improved TileLayer attribution

--- Projects/Uber-Api/frontend/src/features/home/components/ride-map.jsx
+++ Projects/Uber-Api/frontend/src/features/home/components/ride-map.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { MapContainer, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
-const DEFAULT_CENTER = [28.6139, 77.209]
+const DEFAULT_CENTER = [24.8607, 67.0011] // Karachi
 const DEFAULT_ZOOM = 12
 
 const RideMap = () => {
@@ -14,8 +14,8 @@ const RideMap = () => {
       className='size-full z-0'
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
       />
     </MapContainer>
   )
