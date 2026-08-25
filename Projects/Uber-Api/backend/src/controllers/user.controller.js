51780127448204ca26e390fc469703f64ad986e6
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-25 15:41:45 +0500

    feat: enhance user authentication by implementing login functionality with validation and error handling

--- Projects/Uber-Api/backend/src/controllers/user.controller.js
+++ Projects/Uber-Api/backend/src/controllers/user.controller.js
@@ -24,14 +24,39 @@ export const registerUser = async (req, res) => {
         });
 
         const token = user.generateAuthToken();
-        res.status(201).json({user ,token});
+        res.status(201).json({message: "User created successfully", user, token});
     } catch (error) {
         console.log(error)
         res.status(500).json({message: "Internal server error"});
     }
 }
 
-export const loginUser = async (req, res) => {}
+export const loginUser = async (req, res) => {
+    try {
+        const error = validationResult(req);
+        if(!error.isEmpty()) {
+            return res.status(400).json({errors: error.array()});
+        }
+
+        const {email, password} = req.body;
+        const user = await userModel.findOne({email}).select("+password");
+        if(!user){
+            return res.status(400).json({message: "Invalid email or password"});
+        }
+
+        const isMatched = await user.comparePassword(password);
+        if(!isMatched){
+            return res.status(400).json({message: "Invalid email or password"});
+        }
+
+        const token = user.generateAuthToken();
+        res.cookie("token", token) 
+        res.status(200).json({message: "Login successful", user, token});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
 
 export const getUserProfile = async (req, res) => {}
 
