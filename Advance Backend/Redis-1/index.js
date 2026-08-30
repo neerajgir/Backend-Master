2cd62be53129fe407dec49f303f50263f34f93d4
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-30 23:37:22 +0500

    feat: implement user creation and retrieval with Redis caching

--- Advance Backend/Redis-1/index.js
+++ Advance Backend/Redis-1/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config()
 import { connectDB } from "./configs/db.js"
+import User from "./models/user.model.js"
+import client from "./configs/client.js"
 
 const PORT = process.env.PORT || 5000
 
@@ -10,10 +12,28 @@ app.use(express.json());
 
 
 
-app.get('/', (res,req)=>{
-  res.send('hello from redis')  
+// app.get("/", (req,res)=>{
+//   return res.status(200).json({message: "hello from redis"}) 
+// })
+
+app.post("/create", async (req,res) => {
+    const {name, email, password} =req.body;
+    await client.del("user:all")
+    const user = await User.create({name, email, password})
+    return res.status(200).json({user})
 })
 
+app.get("/redis-get", async (req,res) => {
+    const cached = await client.get("user:all")
+    if(cached){
+        const user = JSON.parse(cached)
+        return res.json(user)
+    }
+
+    const user = await User.find({})
+    await client.set("user:all", JSON.stringify(user))
+    return res.json({user})
+})
 
 connectDB();
 app.listen(PORT, ()=>{
