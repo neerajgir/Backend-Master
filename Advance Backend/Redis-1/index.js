51b69ffc433012b87911d336decce7b7bb93b0c7
Neeraj Gir <neerajgoswami871@gmail.com>
2026-08-30 23:55:57 +0500

    feat: implement OTP generation and verification endpoints

--- Advance Backend/Redis-1/index.js
+++ Advance Backend/Redis-1/index.js
@@ -11,11 +11,7 @@ const app = express();
 app.use(express.json());
 
 
-
-// app.get("/", (req,res)=>{
-//   return res.status(200).json({message: "hello from redis"}) 
-// })
-
+//api-caching
 app.post("/create", async (req,res) => {
     const {name, email, password} =req.body;
     await client.del("user:all")
@@ -34,6 +30,28 @@ app.get("/redis-get", async (req,res) => {
     await client.set("user:all", JSON.stringify(user))
     return res.json({user})
 })
+
+//otp
+app.post("/send-otp", async (req,res) => {
+    const {email} =req.body;
+    const otp = Math.floor(100000+Math.random()*900000).toString();
+    
+    await client.set(`otp:${email}`, otp, "EX",30);
+    
+    res.status(200).json({otp})
+})
+app.post("/verify-otp", async (req,res) => {
+    const {email, otp} =req.body;
+    
+    const cachedOtp = await client.get(`otp:${email}`);
+    if(!cachedOtp) return res.json({message: "otp expire"})
+
+    if(cachedOtp != otp) return res.status(400).json({message: "Incorrect OTP"})
+
+    res.status(200).json({message: "otp verified"})
+})
+
+
 
 connectDB();
 app.listen(PORT, ()=>{
