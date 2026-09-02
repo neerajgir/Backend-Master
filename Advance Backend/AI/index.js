34e2828514273c6f30f7b613ac9ce82abfc1d634
Neeraj Gir <neerajgoswami871@gmail.com>
2026-09-02 15:39:03 +0500

    refactor: update content generation endpoint to use dynamic prompt input

--- Advance Backend/AI/index.js
+++ Advance Backend/AI/index.js
@@ -8,26 +8,45 @@ const genAI = new GoogleGenAI({
     apiKey: process.env.GOOGLE_API_KEY,
 });
 
-const interaction = async () => {
-  const response = await genAI.models.generateContent({
-    model: "gemini-3.5-flash",
-    contents: [{
-      role: "user",
-      parts: [{
-        text: "Write a story about a magic backpack."
-      }]
-    }]
-  });
+// const interaction = async () => {
+//   const response = await genAI.models.generateContent({
+//     model: "gemini-3.5-flash",
+//     contents: [{
+//       role: "user",
+//       parts: [{
+//         text: "Write a story about a magic backpack."
+//       }]
+//     }]
+//   });
 
-  console.log(response.text);
-}
+//   console.log(response.text);
+// }
 
-interaction();
+// interaction();
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+
+
+app.post("/generate", async (req, res) => {
+  const { prompt } = req.body;
+
+  const response = await genAI.models.generateContent({
+    model: "gemini-3.5-flash",
+    contents: [{
+      role: "user",
+      parts: [{
+        text: prompt
+      }]
+    }]
+  });
+
+  res.status(200).json({ response: response.text });
 });
 
 app.listen(PORT, () => {
