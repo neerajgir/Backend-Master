5f9c22f81e9d899a6f3387a342dba17231638eb5
Neeraj Gir <neerajgoswami871@gmail.com>
2026-09-02 15:21:59 +0500

    feat: integrate Google GenAI and dotenv for content generation

--- Advance Backend/AI/index.js
+++ Advance Backend/AI/index.js
@@ -1,6 +1,28 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import {GoogleGenAI} from "@google/genai"
 
 const app = express();
+const genAI = new GoogleGenAI({
+    apiKey: process.env.GOOGLE_API_KEY,
+});
+
+const interaction = async () => {
+  const response = await genAI.models.generateContent({
+    model: "gemini-3.5-flash",
+    contents: [{
+      role: "user",
+      parts: [{
+        text: "Write a story about a magic backpack."
+      }]
+    }]
+  });
+
+  console.log(response.text);
+}
+
+interaction();
 
 const PORT = process.env.PORT || 3000;
 
