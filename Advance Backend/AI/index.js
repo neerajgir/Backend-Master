899209f057327288f813c12101476933edef0b34
Neeraj Gir <neerajgoswami871@gmail.com>
2026-09-05 02:29:54 +0500

    feat: add Tavily integration and update dependencies for enhanced tool functionality

--- Advance Backend/AI/index.js
+++ Advance Backend/AI/index.js
@@ -4,8 +4,10 @@ dotenv.config();
 import {GoogleGenAI} from "@google/genai"
 import {ChatGoogleGenerativeAI} from "@langchain/google-genai";
 import { ChatGroq } from "@langchain/groq";
-import { Annotation,StateGraph, END, START,  } from "@langchain/langgraph";
-
+import { Annotation,StateGraph, END, START, MessagesAnnotation, MemorySaver  } from "@langchain/langgraph";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { TavilySearch } from "@langchain/tavily";
+import { SystemMessage } from "@langchain/core/messages";
 const app = express();
 const genAI = new GoogleGenAI({
     apiKey: process.env.GOOGLE_API_KEY,
@@ -53,45 +55,83 @@ app.get("/", (req, res) => {
 // });
 
 
+const tool = new TavilySearch({
+  apiKey: process.env.TAVILY_API_KEY,
+  maxResults: 5,
+  topic: "general"
+});
+
+const checkPointer = new MemorySaver();
+
+const tools = [tool];
+const toolNode = new ToolNode(tools)
+
+
 const llm = new ChatGroq({
     model: "openai/gpt-oss-120b",
     apiKey: process.env.GROQ_API_KEY,
     temperature: 0.7,
-    maxTokens: 100,
+    maxTokens: 2080,
     maxRetries: 2,
-});
+}).bindTools(tools);
 
 // langraph
 
-const State = Annotation.Root({
-   prompt: Annotation,
-   aiMsg: Annotation,
-})
+// const State = Annotation.Root({
+//    prompt: Annotation,
+//    aiMsg: Annotation,
+// })
+
+
 
 const callLLM = async (state) => {
   console.log("State received in callLLM:", state);
   const response = await llm.invoke([
-    {role: "system", content: "You are a helpful assistant."},
-    {role: "human", content: state.prompt}
+    {role: "system", content: "You are a helpful assistant, and you have access to a search tool. If the user asks for information, use the search tool to find relevant information and provide it in your response, do not use tools for simple conversation, memory-based questions, greetings, or personal context."},
+    // {role: "human", content: state.prompt}
+    // {role: "user", content: state.messages[0].content }
+    ...state.messages
   ]);
+ 
 
-  return { aiMsg: response.content };
+  // return { aiMsg: response.content };
+  return { messages: [response] };
 }
 
-const graph = new StateGraph(State)
-.addNode("agent", callLLM) 
+const shouldContinue = async (state) => {
+  const lastMessage = state.messages[state.messages.length - 1];
+  if (lastMessage.tool_calls && lastMessage.tool_calls.length > 0) {
+    return "tools";
+  }
+  return END;
+}
+
+const graph = new StateGraph(MessagesAnnotation)
+.addNode("agent", callLLM)
+.addNode("tools", toolNode)
 .addEdge(START, "agent")
-.addEdge("agent", END)
-.compile();
+.addEdge("tools", "agent")
+.addConditionalEdges("agent", shouldContinue)
+// .addEdge("agent", END)
+.compile({checkPointer: checkPointer});
 
 
 app.post("/generate", async (req, res) => {
   const { prompt } = req.body;
 
-  const response = await graph.invoke({ prompt });
+  // const response = await graph.invoke({ prompt });
+  const response = await graph.invoke({ messages:[
+  { role: "user", 
+    content: prompt
+  }
+  ]},
+  {configurable:{threadId: "user1"}}
+);
 
-  res.status(200).json({ response: response.aiMsg });
+  // res.status(200).json({ response: response.aiMsg });
+  res.status(200).json({ response: response.messages[response.messages.length - 1].content }); 
 });
+
 
 // app.post("/generate", async (req, res) => {
 //   const { prompt } = req.body;
