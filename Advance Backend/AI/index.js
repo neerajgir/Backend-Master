ae30734728038fafcc04e7680a75cdee497e494d
Neeraj Gir <neerajgoswami871@gmail.com>
2026-09-05 01:27:45 +0500

    feat: integrate langgraph for enhanced state management in content generation

--- Advance Backend/AI/index.js
+++ Advance Backend/AI/index.js
@@ -4,6 +4,7 @@ dotenv.config();
 import {GoogleGenAI} from "@google/genai"
 import {ChatGoogleGenerativeAI} from "@langchain/google-genai";
 import { ChatGroq } from "@langchain/groq";
+import { Annotation,StateGraph, END, START,  } from "@langchain/langgraph";
 
 const app = express();
 const genAI = new GoogleGenAI({
@@ -60,16 +61,48 @@ const llm = new ChatGroq({
     maxRetries: 2,
 });
 
+// langraph
+
+const State = Annotation.Root({
+   prompt: Annotation,
+   aiMsg: Annotation,
+})
+
+const callLLM = async (state) => {
+  console.log("State received in callLLM:", state);
+  const response = await llm.invoke([
+    {role: "system", content: "You are a helpful assistant."},
+    {role: "human", content: state.prompt}
+  ]);
+
+  return { aiMsg: response.content };
+}
+
+const graph = new StateGraph(State)
+.addNode("agent", callLLM) 
+.addEdge(START, "agent")
+.addEdge("agent", END)
+.compile();
+
+
 app.post("/generate", async (req, res) => {
   const { prompt } = req.body;
 
-  const response = await llm.invoke([
-    {role: "system", content: "You are a helpful assistant."},
-    {role: "human", content: prompt}
-  ]);
+  const response = await graph.invoke({ prompt });
 
-  res.status(200).json({ response: response.content });
+  res.status(200).json({ response: response.aiMsg });
 });
+
+// app.post("/generate", async (req, res) => {
+//   const { prompt } = req.body;
+
+//   const response = await llm.invoke([
+//     {role: "system", content: "You are a helpful assistant."},
+//     {role: "human", content: prompt}
+//   ]);
+
+//   res.status(200).json({ response: response.content });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
