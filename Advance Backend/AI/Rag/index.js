09cd1b5b19fb245bd0ee407c0a03af887564c62e
Neeraj Gir <neerajgoswami871@gmail.com>
2026-09-06 00:51:52 +0500

    initialize pdf extractions and divide them into chunks

--- Advance Backend/AI/Rag/index.js
+++ Advance Backend/AI/Rag/index.js
@@ -3,6 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 import { ChatGroq } from "@langchain/groq";
 import {ChatGoogleGenerativeAI} from "@langchain/google-genai";
+import fs from "fs";
+import { PDFParse } from "pdf-parse";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import {RecursiveCharacterTextSplitter} from "@langchain/textsplitters"
 
 
 
@@ -15,19 +20,39 @@ const llm = new ChatGroq({
     model: "openai/gpt-oss-120b",
     apiKey: process.env.GROQ_API_KEY,
     temperature: 0.7,
-    maxTokens: 2080,
+    maxTokens: 100,
     maxRetries: 2,
 })
+
+// rag - retrieval augmented generation
+// extract text from pdf
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const upload = async (req, res) => {
+    const pdfPath = path.join(__dirname, 'knowledge.pdf');
+    const buffer = fs.readFileSync(pdfPath);
+    const pdfResult = new PDFParse({ data: buffer });
+    const result = await pdfResult.getText();
+    const text = result.text;
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1000,
+        chunkOverlap: 200,
+    });
+    const doc = await splitter.createDocuments([text])
+    console.log(doc);
+}
+upload();
 
 app.post("/generate", async (req, res) => {
   const { prompt } = req.body;
 
-  const response = await llm.invoke({ messages:[
+  const response = await llm.invoke([
   { role: "user", 
     content: prompt
   }
-  ]}
-);
+  ]);
 
   res.status(200).json({ response: response.content });
 });
